c6c59c340c955923a01aa5453a39d4c474d1a9ec
osama1225 <eng.osama12@gmail.com>
2016-02-12 20:43:13 +0200

    Add Listener class for finish loading playlist.

---
+++ app/src/main/java/viewer/media/android/androidmediaviewer/Utils/FinishedLoadingPlaylistListener.java
@@ -0,0 +1,9 @@
+package viewer.media.android.androidmediaviewer.Utils;
+
+/**
+ * Created by Mohamed on 2/12/2016.
+ */
+public interface FinishedLoadingPlaylistListener {
+
+    public void onFinished();
+}
