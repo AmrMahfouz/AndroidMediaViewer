188ebfe3b89ef15c51b37a61670b586f1c0d624c
osama1225 <eng.osama12@gmail.com>
2016-02-12 20:46:41 +0200

    Edit to that videos start playing after playlist is loaded

--- app/src/main/java/viewer/media/android/androidmediaviewer/Controller/MainActivity.java
+++ app/src/main/java/viewer/media/android/androidmediaviewer/Controller/MainActivity.java
@@ -2,6 +2,7 @@ package viewer.media.android.androidmediaviewer.Controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import viewer.media.android.androidmediaviewer.Core.CrashHandler;
 import viewer.media.android.androidmediaviewer.Core.VideoScanner;
 import viewer.media.android.androidmediaviewer.R;
+import viewer.media.android.androidmediaviewer.Utils.FinishedLoadingPlaylistListener;
 import viewer.media.android.androidmediaviewer.Utils.WakeLocker;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,14 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
         //Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(this, MainActivity.class));
 
-        VideoScanner videoScanner = new VideoScanner(getApplicationContext());
-        ArrayList<String> playList = videoScanner.getPlayList();
-
-        VideoView videoView = (VideoView) findViewById(R.id.videoPlayer);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        final VideoView videoView = (VideoView) findViewById(R.id.videoPlayer);
         videoView.requestFocus();
 
-        MediaViewer mediaViewer = new MediaViewer(getApplicationContext(), videoView, playList);
-        mediaViewer.initPlayer();
+        final VideoScanner videoScanner = new VideoScanner(getApplicationContext(),progressBar);
+        videoScanner.setOnFinishedLoadingPlaylistListener(new FinishedLoadingPlaylistListener() {
+            @Override
+            public void onFinished() {
+                ArrayList<String> playList = videoScanner.getPlayList();
+                MediaViewer mediaViewer = new MediaViewer(getApplicationContext(), videoView, playList);
+                mediaViewer.initPlayer();
+            }
+        });
 
         WakeLocker.acquire(getApplicationContext());
     }
