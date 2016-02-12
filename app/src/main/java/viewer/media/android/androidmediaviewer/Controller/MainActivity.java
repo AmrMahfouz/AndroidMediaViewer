9c19c32d10a0ea17da02f005369ecc87e588ab94
AmrMahfouz <eng.csed.amr.mahfouz@gmail.com>
2016-02-12 16:24:55 +0200

    Play the loaded videos

--- app/src/main/java/viewer/media/android/androidmediaviewer/Controller/MainActivity.java
+++ app/src/main/java/viewer/media/android/androidmediaviewer/Controller/MainActivity.java
@@ -2,6 +2,7 @@ package viewer.media.android.androidmediaviewer.Controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
 
 import java.util.ArrayList;
 
@@ -17,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         VideoScanner videoScanner = new VideoScanner(getApplicationContext());
         ArrayList<String> playList = videoScanner.getPlayList();
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoPlayer);
+        videoView.requestFocus();
+
+        MediaViewer mediaViewer = new MediaViewer(getApplicationContext(), videoView, playList);
+        mediaViewer.initPlayer();
+
         Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(this, MainActivity.class));
     }
 
