e65f3293aecf6bd198096b0922374d14900146ca
AmrMahfouz <eng.csed.amr.mahfouz@gmail.com>
2016-02-12 19:35:04 +0200

    Support all storage types internal, external, usb

--- app/src/main/java/viewer/media/android/androidmediaviewer/Core/VideoScanner.java
+++ app/src/main/java/viewer/media/android/androidmediaviewer/Core/VideoScanner.java
@@ -2,10 +2,14 @@ package viewer.media.android.androidmediaviewer.Core;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v4.content.CursorLoader;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Amr Mahfouz on 2/12/2016.
@@ -25,32 +29,34 @@ public class VideoScanner {
     }
 
     private void scanVideos() {
-        // scan internal storage
-        String[] projection = { MediaStore.Video.Media.DISPLAY_NAME, MediaStore.Video.Media.DATA, MediaStore.Video.Media.SIZE };
-        Cursor cursor = new CursorLoader(context, MediaStore.Video.Media.INTERNAL_CONTENT_URI, projection, null, null, null).loadInBackground();
-        while (cursor.moveToNext()) {
-            String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
-            if (videoNames.contains(name)) {
-                String path = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
-                int videoIndex = videoNames.indexOf(name);
-                videoNames.remove(videoIndex);
-                videoNames.add(videoIndex, path);
-            }
-        }
-        cursor.close();
 
-        // scan external storage
-        cursor = new CursorLoader(context, MediaStore.Video.Media.EXTERNAL_CONTENT_URI, projection, null, null, null).loadInBackground();
-        while (cursor.moveToNext()) {
-            String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
-            if (videoNames.contains(name)) {
-                String path = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
-                int videoIndex = videoNames.indexOf(name);
-                videoNames.remove(videoIndex);
-                videoNames.add(videoIndex, path);
+        String defaultStoragePath = System.getenv("EXTERNAL_STORAGE");
+        String[] path = defaultStoragePath.split("/");
+        if (path != null && path.length >= 2) {
+            String storageRoot = defaultStoragePath.split("/")[1];
+            File folder_file = new File("/" + storageRoot + "/");
+            loadVideoFiles(folder_file);
+        }
+    }
+
+    private void loadVideoFiles(File folder) {
+        File[] files = folder.listFiles();
+        if (files == null) {
+            return;
+        }
+        for (File file : files) {
+            if (file.isDirectory()) {
+                loadVideoFiles(file);
+            } else if (file.isFile()){
+                String fileName = file.getName();
+                if (videoNames.contains(fileName)) {
+                    String path = file.getAbsolutePath();
+                    int videoIndex = videoNames.indexOf(fileName);
+                    videoNames.remove(videoIndex);
+                    videoNames.add(videoIndex, path);
+                }
             }
         }
-        cursor.close();
     }
 
     public ArrayList<String> getPlayList() {
