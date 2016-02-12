e38650d4afa0d699d1a1c7136a5f55d0854c616f
osama1225 <eng.osama12@gmail.com>
2016-02-12 20:44:25 +0200

    load the playlist in background. notify receiver when loading is finished.

--- app/src/main/java/viewer/media/android/androidmediaviewer/Core/VideoScanner.java
+++ app/src/main/java/viewer/media/android/androidmediaviewer/Core/VideoScanner.java
@@ -1,31 +1,50 @@
 package viewer.media.android.androidmediaviewer.Core;
 
 import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v4.content.CursorLoader;
-import android.widget.Toast;
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
+
+import viewer.media.android.androidmediaviewer.Utils.FinishedLoadingPlaylistListener;
 
 /**
  * Created by Amr Mahfouz on 2/12/2016.
  */
-public class VideoScanner {
+public class VideoScanner extends AsyncTask<Void,Void,Void>  {
 
     private Context context;
-
+    private ProgressBar progressBar;
     private PlaylistLoader playlistLoader;
     private ArrayList<String> videoNames;
 
-    public VideoScanner(Context context) {
+
+    private FinishedLoadingPlaylistListener onFinishedLoadingPlaylistListener;
+    public VideoScanner(Context context,ProgressBar progressBar) {
         this.context = context;
+        this.progressBar = progressBar;
         playlistLoader = new PlaylistLoader();
         videoNames = playlistLoader.getVideosNames();
+        execute(new Void[]{});
+    }
+
+    @Override
+    protected void onPreExecute() {
+        progressBar.setVisibility(ProgressBar.VISIBLE);
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
         scanVideos();
+        return null;
+    }
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
+        notifyListener();
     }
 
     private void scanVideos() {
@@ -62,4 +81,15 @@ public class VideoScanner {
     public ArrayList<String> getPlayList() {
         return videoNames;
     }
+
+    public void setOnFinishedLoadingPlaylistListener(FinishedLoadingPlaylistListener onFinishedLoadingPlaylistListener){
+        this.onFinishedLoadingPlaylistListener = onFinishedLoadingPlaylistListener;
+    }
+
+    private void notifyListener(){
+        if(onFinishedLoadingPlaylistListener != null){
+            onFinishedLoadingPlaylistListener.onFinished();
+        }
+    }
+
 }
